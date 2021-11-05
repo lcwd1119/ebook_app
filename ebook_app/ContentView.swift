c6fb8cee8e4ded59f1c2d7faa25dd1152c996e88
David <lcwd1119@gmail.com>
2021-11-05 23:11:39 +0800

    Initial Commit

---
+++ ebook_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ebook_app
+//
+//  Created by 廖晨維 on 2021/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ebook_app/ebook_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebook_appApp.swift
+//  ebook_app
+//
+//  Created by 廖晨維 on 2021/11/5.
+//
+
+import SwiftUI
+
+@main
+struct ebook_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
